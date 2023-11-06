664cb98d75d7bd9076b4835408abd49a7eb69b17
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-11-06 10:06:48 +0200

    feat(encryption): implement caesar cipher encryption in functional paradigm

---
+++ main.py
@@ -0,0 +1,19 @@
+def encrypt(rawText: str, key: int) -> str:
+    result: str = ''
+    for char in rawText:
+        if char == '\0':
+            break
+
+        if char.isalpha():
+            base = 'A' if char.isupper() else 'a'
+            result += chr(((ord(char) - ord(base) + key) % 26) + ord(base))
+        else:
+            result += chr((ord(char) + key) % 68)
+        
+    return result
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
